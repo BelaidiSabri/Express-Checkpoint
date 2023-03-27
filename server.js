1ac9b7626a78ea4478c664912d1fb9223183ebd8
Belaidi Sabri <belaidisabri@gmail.com>
2023-03-27 14:44:11 +0100

    Add Time Left

--- server.js
+++ server.js
@@ -10,23 +10,31 @@ app.use((req,res,next) => {
     const date = new Date();
     const day = date.getDay(); 
     const hour = date.getHours();
-    //req.workinghours=(day >= 1 && day <= 5 && hour >= 9 && hour <= 17)    //here test closing hours
-    req.workinghours=(day >= 0 && day <= 6 && hour >= 00 && hour <= 23)     //here test opening hours
+    const minute = date.getMinutes();
+    if(day >= 1 && day <= 5){
+        req.day = 0
+    } else if (day===6){
+        req.day=2
+    }
+    else{req.day=1}
+    req.hour = 17-hour
+    req.minute = 60-minute;
+    req.workinghours=(day >= 1 && day <= 5 && hour >= 17 && hour <= 22)    //here test closing hours
+    //req.workinghours=(day >= 0 && day <= 6 && hour >= 00 && hour <= 23)     //here test opening hours
     next()
   
   });
  
 
 app.get('/',(req,res)=>{
-    res.render('home', { workinghours: req.workinghours })
+    res.render('home', { workinghours: req.workinghours ,day:req.day, hour :req.hour, minute :req.minute});
 })
-
 app.get('/services',(req,res)=>{
-    res.render(__dirname+'/views/services.ejs',{ workinghours: req.workinghours })
+    res.render(__dirname+'/views/services.ejs',{ workinghours: req.workinghours ,day:req.day, hour :req.hour, minute :req.minute})
 })
 
 app.get('/contact',(req,res)=>{
-    res.render(__dirname+'/views/contact.ejs',{ workinghours: req.workinghours })
+    res.render(__dirname+'/views/contact.ejs',{ workinghours: req.workinghours ,day:req.day, hour :req.hour, minute :req.minute})
 })
 app.listen(port, console.log('App is live now on port ' + port));
 
---
+++ views/function.js
@@ -0,0 +1,14 @@
+const calc = () => {
+
+     dayRest = dayopening-currentDay
+     minuteRest = Minuteopening- currentMinute 
+
+
+    console.log(`still dayRest : hourRest:  minuteRest:  to open`);
+
+  return (
+    <div>function</div>
+  )
+}
+
+export default calc()
